0022e692424d96e820e87b8d065471f3b1658e45
unknown <sadeeshasanhinda57@gmail.com>
2023-01-10 21:00:19 +0530

    Add backend & Now We can convert video to mp3

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const axios = require("axios");
 
 const app = express();
 
-// app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
@@ -14,16 +13,10 @@ app.get("/", function(req, res){
     res.sendFile(__dirname + "/index.html");
 });
 
-// var userUrl = "";
 
-// var mp3 = "";
-// // var thumbnail = "";
-// var duration = "";
-// var title = "";
 
 app.post("/button", function(req,res){
-    // var userUrl = req.body.url;
-    
+
     let url = req.body.url;
     const regex = new RegExp('=(.+)$')
 
@@ -58,12 +51,10 @@ app.post("/button", function(req,res){
         response.then(function (rsp) {
           var apiDetails = {
             mp3Url: rsp.data.dlink,
-            // thumbnailUrl: thumbnail,
             durationTime: duration,
             videoTitle: rsp.data.title
           };
             res.send(apiDetails);
-            console.log(apiDetails);
             
 
         })
@@ -72,30 +63,7 @@ app.post("/button", function(req,res){
     
 });
 
-// const options = {
-//     method: 'GET',
-//     url: 'https://t-one-youtube-converter.p.rapidapi.com/api/v1/createProcess',
-//     params: {
-//       url: userUrl,
-//       format: 'mp3',
-//       responseFormat: 'json',
-//       lang: 'en'
-//     },
-//     headers: {
-//       'X-RapidAPI-Key': '3bb4058cccmsh4c39876ee145b04p124dcajsn6698ba300f28',
-//       'X-RapidAPI-Host': 't-one-youtube-converter.p.rapidapi.com'
-//     }
-//   };
-  
-//   axios.request(options).then(function (response) {
-//         mp3 = response.data.YoutubeAPI.urlMp3;
-//         thumbnail = response.data.YoutubeAPI.thumbUrl;
-//         duration = response.data.YoutubeAPI.durata_video;
-//         title = response.data.YoutubeAPI.titolo;
-//   }).catch(function (error) {
-//         console.error(error);
-//   });
 
-app.listen(3000, function(){
-    console.log("Server Started on Successfully");
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Server Started Successfully");
 });
--- public/js/index.js
+++ public/js/index.js
@@ -5,7 +5,6 @@ function onFormSubmit() {
 };
 
 var mp3 = "";
-// var thumbnail = "";
 var duration = "";
 var title = ""
 
@@ -18,50 +17,63 @@ $("#spin").addClass("js-hide");
         let a = document.createElement("a");
         a.setAttribute("href", value)
         var thumbnailCode = a.search;
+        var validate = a.pathname;
         thumbnailCode = thumbnailCode.slice(3, thumbnailCode.length);
         var thumbnailUrl = "https://i.ytimg.com/vi/" + thumbnailCode + "/0.jpg"
-        console.log(thumbnailUrl);
+        console.log(thumbnailUrl, validate);
 
-        $.ajax({
-            type: "POST",
-            url: "/button",
-            data: {url: value},
-            success: function(res){
-                mp3 = res.mp3Url;
-                // thumbnail = res.thumbnailUrl;
-                duration = res.durationTime;
-                title = res.videoTitle;
-
-                var min = Math.floor(duration / 60);
-                var sec = duration % 60;
-
-                $("#title").text(title);
-                $("#duration").text("Duration: " + min + ":" + sec);
-                $("#download").attr("href", mp3);
-                $("#thumbnail").attr("src", thumbnailUrl);
-                console.log(min + " " + sec);
-            }
-        });
-        
-
-
-        if ($("#spin").attr("class") === "hide-spin" || $("#spin").attr("js") === "js-hide" ){
-            console.log("Already Aplied");
-        } else {
-            $("#spin").removeClass("js-hide");
-            $("#spin").addClass("spinner");
-        
-            setInterval(function(){
+        if (validate === "/watch"){
+            $(".error").text("")
+            $.ajax({
+                type: "POST",
+                url: "/button",
+                data: {url: value},
+                success: function(res){
+                    mp3 = res.mp3Url;
+                    duration = res.durationTime;
+                    title = res.videoTitle;
+    
+                    var min = Math.floor(duration / 60);
+                    var sec = duration % 60;
+    
+                    $("#title").text(title);
+                    $("#duration").text("Duration: " + min + ":" + sec);
+                    $("#download").attr("href", mp3);
+                    $("#thumbnail").attr("src", thumbnailUrl);
+                    console.log(min + " " + sec);
+                }
+            });
+            
+    
+    
+            if ($("#spin").attr("class") === "hide-spin" || $("#spin").attr("js") === "js-hide" ){
+                console.log("Already Aplied");
+            } else {
                 $("#spin").removeClass("js-hide");
-                $('#js').slideDown();
-            },10000);
-        
-            setTimeout(function(){
-                $("#spin").removeClass("spinner");
-            }, 10000);  
+                $("#spin").addClass("progress");
+                
+                setTimeout(function(){
+                    $("#spin").removeClass("progress");
+                }, 15000); 
+    
+    
+                setInterval(function(){
+                    $("#spin").removeClass("js-hide");
+                    $('#js').slideDown();
+                },15000);
+                
+                 
+            }
+            console.log(validate);
+        } else {
+            $(".error").text("Please Enter Valid link")
+            console.log("fuck" + validate);
         }
+
+        
         
     });
+
 
 
 
